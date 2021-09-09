6e5e720916e9d2c51e0e1b0c36d89005adeb699d
Klortak <84869525+Klortak@users.noreply.github.com>
2021-09-09 08:26:40 -0500

    Add files via upload

---
+++ draggable.js
@@ -0,0 +1,68 @@
+var drop = document.getElementById("drop-zone")
+var drag = document.getElementById("draggable")
+var lights = document.getElementById("led")
+
+dragElement(drag);
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  elmnt.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+    touches(drop, drag)
+  }
+}
+
+function touches(el1, el2) {
+  const rect1 = el1.getBoundingClientRect();
+  const rect2 = el2.getBoundingClientRect();
+
+  if(rect1.x < rect2.x + rect2.width &&
+    rect1.x + rect1.width > rect2.x &&
+    rect1.y < rect2.y + rect2.height &&
+    rect1.y + rect1.height > rect2.y){
+      on()
+    }
+    else {
+      off()
+    }
+}
+
+function on() {
+  lights.classList.remove("off")
+  lights.classList.add("on")
+  lights.style.animation = "1s flicker 1"
+}
+
+function off() {
+  lights.classList.remove("on")
+  lights.classList.add("off")
+  lights.style.animation = "1s off 1"
+}
